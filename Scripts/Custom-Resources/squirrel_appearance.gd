87caffa793550b9a1b510b6bb552c717b8df53a0
Thanasis <thanasishadow@gmail.com>
2025-03-17 22:44:56 +0200

    Updated the squirrel character system.
    Squirrels can now be created and saved easily, with the "Squirrel_Appearance" resource.
    
    Placeholders for most textures have been put in place, for now.

--- Scripts/Custom-Resources/squirrel_appearance.gd
+++ Scripts/Custom-Resources/squirrel_appearance.gd
@@ -1,39 +1,98 @@
+@tool
 extends Resource
 class_name Squirrel_Appearance
 
+signal updated_sprites
+signal updated_transforms
+signal updated_name
+
 @export_category("Base Characteristics")
-@export var base: Squirrel_Base_Textures
-@export_range(0.4, 1.4) var body_size: float = 1
+@export var base: Squirrel_Base_Textures:
+	set(val):
+		base = val
+		_update_sprites()
+@export_range(0.4, 1.4) var body_size: float = 1:
+	set(val):
+		body_size = val
+		_update_transforms()
 
-@export_enum ("Simple", "Beady", "Cartoony", "Creepy", "Oblivious", "Dead", "Closed", "Red") var eye_texture: int
-@export_range(0.2, 2.0) var eye_size: float = 1
-@export_group("Eye Offset")
-@export var eye_offset_X: float = 0
-@export var eye_offset_Y: float = 0
-@export_group("")
+@export_enum ("Simple", "Beady", "Cartoony", "Creepy", "Oblivious", "Dead", "Closed", "Red") var eye_texture: int:
+	set(val):
+		eye_texture = val
+		_update_sprites()
+@export_range(0.2, 2.0) var eye_size: float = 1:
+	set(val):
+		eye_size = val
+		_update_transforms()
+@export var eye_offset: Vector2 = Vector2.ZERO:
+	set(val):
+		eye_offset = val
+		_update_transforms()
 
-@export_enum ("Simple", "Bushy", "Extra Bushy", "Fine", "Cartoony", "Distinguished", "Rough", "None") var eyebrow_texture: int
-@export_range(0.3, 2.0) var eyebrow_size: float = 1
-@export_group("Eyebrow Offset")
-@export var eyebrow_offset_X: float = 0
-@export var eyebrow_offset_Y: float = 0
-@export_group("")
+@export_enum ("Simple", "Bushy", "Extra Bushy", "Fine", "Cartoony", "Distinguished", "Rough", "None") var eyebrow_texture: int:
+	set(val):
+		eyebrow_texture = val
+		_update_sprites()
+@export_range(0.3, 2.0) var eyebrow_size: float = 1:
+	set(val):
+		eyebrow_size = val
+		_update_transforms()
+@export var eyebrow_offset: Vector2 = Vector2.ZERO:
+	set(val):
+		eyebrow_offset = val
+		_update_transforms()
 
 @export_category("Accessories")
-@export_enum ("Crown", "Top Hat", "Flowers", "Turd", "Sprout", "Artistic", "Bow", "None") var hat_texture: int
+@export_enum ("Crown", "Top Hat", "Flowers", "Turd", "Chef's Hat", "Artistic", "Bow", "NONE") var hat_texture: int:
+	set(val):
+		hat_texture = val
+		_update_sprites()
 
-@export_enum ("Scarf", "Necktie", "Badge", "Belt", "Necklace", "Jabot", "Bow", "None") var accessory_texture: int
-@export_group("Accessory Offset")
-@export var accessory_offset_X: float = 0
-@export var accessory_offset_Y: float = 0
-@export_group("")
+@export_enum ("Scarf", "Necktie", "Badge", "Belt", "Necklace", "Jabot", "Bow", "?", "NONE") var accessory_texture: int:
+	set(val):
+		accessory_texture = val
+		_update_sprites()
+@export var accessory_offset: Vector2 = Vector2.ZERO:
+	set(val):
+		accessory_offset = val
+		_update_transforms()
 
-@export_enum ("Sword", "Hammer", "Boquet", "Scepter", "Potion", "Scroll", "Sack", "None") var held_R_texture: int
-@export_group("Held Object Offset (Right Hand)")
-@export var object_R_offset_X: float = 0
-@export var object_R_offset_Y: float = 0
-@export_group("")
-@export_enum ("Sword", "Hammer", "Boquet", "Scepter", "Potion", "Scroll", "Sack", "None") var held_L_texture: int
-@export_group("Held Object Offset (Left Hand)")
-@export var object_L_offset_X: float = 0
-@export var object_L_offset_Y: float = 0
+@export_enum ("Sword", "Hammer", "Boquet", "Scepter", "Potion", "Scroll", "Sack","Quill", "Duster", "Cleaver", "Magic Wand", "Rag", "Acorn", "Basket", "?", "None") var held_r_texture: int:
+	set(val):
+		held_r_texture = val
+		_update_sprites()
+@export var object_r_offset: Vector2 = Vector2.ZERO:
+	set(val):
+		object_r_offset = val
+		_update_transforms()
+@export_enum ("Sword", "Hammer", "Boquet", "Scepter", "Potion", "Scroll", "Sack","Quill", "Duster", "Cleaver", "Magic Wand", "Rag", "Acorn", "Basket", "?", "None") var held_l_texture: int:
+	set(val):
+		held_l_texture = val
+		_update_sprites()
+@export var object_l_offset: Vector2 = Vector2.ZERO:
+	set(val):
+		object_l_offset = val
+		_update_transforms()
+
+@export_category("Save Appearance")
+const save_location: String = "res://Audio-Visual/Character-Appearances/"
+@export var save_name: String = "MagicSquirrelTheFourth":
+	set(val):
+		save_name = val
+		_update_name(val)
+
+@export_tool_button("Save", "Callable") var save_action = save_modified_appearance
+
+func _update_sprites():
+	updated_sprites.emit()
+	
+func _update_transforms():
+	updated_transforms.emit()
+	
+func _update_name(new_name):
+	updated_name.emit(new_name)
+
+func save_modified_appearance():
+	var save_to: String = (save_location + save_name + ".tres")
+	ResourceSaver.save(self, save_to)
+	print("Squirrel " + save_name + " saved to: " + save_to)
--- Scripts/Squirrel-Scene/squirrel_sprite_script.gd
+++ Scripts/Squirrel-Scene/squirrel_sprite_script.gd
@@ -1,1 +1,83 @@
+@tool
 extends Node3D
+
+@export var appearance: Squirrel_Appearance
+
+
+@onready var body: Sprite3D = $Body
+@onready var head: Sprite3D = $Body/Head
+@onready var fluff: Sprite3D = $Body/Fluff
+@onready var arm1_r: Sprite3D = $"Body/Arm1-R"
+@onready var arm2_r: Sprite3D = $"Body/Arm1-R/Arm2-R"
+@onready var hand_r: Sprite3D = $"Body/Arm1-R/Arm2-R/Hand-R"
+@onready var arm1_l: Sprite3D = $"Body/Arm1-L"
+@onready var arm2_l: Sprite3D = $"Body/Arm1-L/Arm2-L"
+@onready var hand_l: Sprite3D = $"Body/Arm1-L/Arm2-L/Hand-L"
+@onready var leg_r: Sprite3D = $"Body/Leg-R"
+@onready var leg_l: Sprite3D = $"Body/Leg-L"
+@onready var tail: Sprite3D = $Body/Tail
+@onready var hat: Sprite3D = $Body/Head/Hat
+@onready var eye_r: Sprite3D = $"Body/Head/Eye-R"
+@onready var eye_l: Sprite3D = $"Body/Head/Eye-L"
+@onready var eyebrow_r: Sprite3D = $"Body/Head/Eyebrow-R"
+@onready var eyebrow_l: Sprite3D = $"Body/Head/Eyebrow-L"
+@onready var accessory: Sprite3D = $Body/Accessory
+@onready var object_l: Sprite3D = $"Body/Arm1-L/Arm2-L/Hand-L/Object-L"
+@onready var object_r: Sprite3D = $"Body/Arm1-R/Arm2-R/Hand-R/Object-R"
+
+@onready var original_eye_l_position: Vector3 = eye_l.position
+@onready var original_eye_r_position: Vector3 = eye_r.position
+@onready var original_eyebrow_l_position: Vector3 = eyebrow_l.position
+@onready var original_eyebrow_r_position: Vector3 = eyebrow_r.position
+@onready var original_accessory_position: Vector3 = accessory.position
+@onready var original_object_l_position: Vector3 = object_l.position
+@onready var original_object_r_position: Vector3 = object_r.position
+
+func _ready():
+	update_all_sprites()
+	update_all_transforms()
+	appearance.updated_sprites.connect(update_all_sprites)
+	appearance.updated_transforms.connect(update_all_transforms)
+
+
+func _process(delta):
+	if not Engine.is_editor_hint():
+		return
+	
+func update_all_sprites():
+	body.texture = appearance.base.body_texture
+	head.texture = appearance.base.head_texture
+	fluff.texture = appearance.base.fluff_texture
+	arm1_r.texture = appearance.base.arm_1_texture
+	arm1_l.texture = appearance.base.arm_1_texture
+	arm2_r.texture = appearance.base.arm_2_texture
+	arm2_l.texture = appearance.base.arm_2_texture
+	hand_r.texture = appearance.base.hand_texture
+	hand_l.texture = appearance.base.hand_texture
+	leg_r.texture = appearance.base.leg_texture
+	leg_l.texture = appearance.base.leg_texture
+	tail.texture = appearance.base.tail_texture
+	
+	hat.texture.region = Rect2i(appearance.hat_texture * 512, 0, 512, 512)
+	eye_r.texture.region = Rect2i(appearance.eye_texture * 64, 0, 64, 64)
+	eye_l.texture.region = Rect2i(appearance.eye_texture * 64, 0, 64, 64)
+	eyebrow_r.texture.region = Rect2i(appearance.eyebrow_texture * 64, 0, 64, 64)
+	eyebrow_l.texture.region = Rect2i(appearance.eyebrow_texture * 64, 0, 64, 64)
+	accessory.texture.region = Rect2i(appearance.accessory_texture * 512, 0, 512, 512)
+	object_l.texture.region = Rect2i(appearance.held_l_texture * 512, 0, 512, 512)
+	object_r.texture.region = Rect2i(appearance.held_r_texture * 512, 0, 512, 512)
+	
+func update_all_transforms():
+	body.scale = Vector3(appearance.body_size, appearance.body_size, 1)
+	eye_l.scale = Vector3(appearance.eye_size, appearance.eye_size, 1)
+	eye_r.scale = Vector3(appearance.eye_size, appearance.eye_size, 1)
+	eyebrow_l.scale = Vector3(appearance.eyebrow_size, appearance.eyebrow_size, 1)
+	eyebrow_r.scale = Vector3(appearance.eyebrow_size, appearance.eyebrow_size, 1)
+	
+	eye_l.position = original_eye_l_position + Vector3(appearance.eye_offset.x, appearance.eye_offset.y, 0)
+	eye_r.position = original_eye_r_position + Vector3(appearance.eye_offset.x * -1, appearance.eye_offset.y, 0)
+	eyebrow_l.position = original_eyebrow_l_position + Vector3(appearance.eyebrow_offset.x, appearance.eyebrow_offset.y, 0)
+	eyebrow_r.position = original_eyebrow_r_position + Vector3(appearance.eyebrow_offset.x * -1, appearance.eyebrow_offset.y, 0)
+	accessory.position = original_accessory_position + Vector3(appearance.accessory_offset.x, appearance.accessory_offset.y, 0)
+	object_l.position = original_object_l_position + Vector3(appearance.object_l_offset.x, appearance.object_l_offset.y, 0)
+	object_r.position = original_object_r_position + Vector3(appearance.object_r_offset.x, appearance.object_r_offset.y, 0)
